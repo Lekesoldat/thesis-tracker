c4d7182b88798953f23ac529186c817a72e63e08
kharann <anhkhav@gmail.com>
2022-09-12 15:14:58 +0200

    add charts

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,64 @@
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Anhkha",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Magnus",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
 export default function Index() {
   return (
-    <div className="bg-blue-400">
+    <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Line data={data} options={options} />
     </div>
   );
 }
